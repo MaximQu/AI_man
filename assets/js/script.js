933658710126ce8e115e36ce7883cfd69f68a21b
Maxim <63422884+MaximQu@users.noreply.github.com>
2023-08-29 14:05:11 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+import headerSticky from './modules/header-sticky.js';
+import burgerMenu from './modules/burger.js';
+import tabs from './modules/tabs.js';
+import supportSlider from './modules/support-slider.js';
+
+headerSticky();
+burgerMenu();
+tabs();
+supportSlider();
