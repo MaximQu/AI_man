9e99b770eef3d5d07f1fb2b4a62879107b875877
Maxim <63422884+MaximQu@users.noreply.github.com>
2023-08-29 14:45:01 +0300

    Delete assets/js directory

--- assets/js/modules/header-sticky.js
+++
@@ -1,9 +0,0 @@
-const headerSticky = () => {
-  const header = document.querySelector('header'),
-    bodyRect = document.body.getBoundingClientRect();
-
-  if (bodyRect.top < 0) header.classList.add('sticky');
-  window.addEventListener('scroll', () => header.classList.toggle('sticky', window.scrollY > 0));
-};
-
-export default headerSticky;
--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-import headerSticky from './modules/header-sticky.js';
-import burgerMenu from './modules/burger.js';
-import tabs from './modules/tabs.js';
-import supportSlider from './modules/support-slider.js';
-
-headerSticky();
-burgerMenu();
-tabs();
-supportSlider();
